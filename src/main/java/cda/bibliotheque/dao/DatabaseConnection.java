d332441aa1d7eefb24d5c33d55e1f31b2ce38081
QuentinG <ZexFrost1@gmail.com>
2025-10-08 16:53:02 +0200

    [~] change database name

--- src/main/java/cda/bibliotheque/dao/DatabaseConnection.java
+++ src/main/java/cda/bibliotheque/dao/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/bibliotheque_cda2025";
+    private static final String URL = "jdbc:mysql://localhost:3306/bibliotheque";
     // private static final String URL = "jdbc:postgresql://localhost:3306/bibliotheque_cda2025";
     private static final String USER = "root";
     private static final String PASSWORD = "";
