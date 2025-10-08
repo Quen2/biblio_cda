14dbc76ff498ab84fb18ba7c2ea33534c8d8703e
QuentinG <ZexFrost1@gmail.com>
2025-10-08 17:00:59 +0200

    [~] WIP book crud

---
+++ src/main/java/cda/bibliotheque/dao/BookDAO.java
@@ -0,0 +1,7 @@
+package cda.bibliotheque.dao;
+
+import java.sql.Connection;
+
+public class BookDAO {
+    private Connection connection;
+}
---
+++ src/main/java/cda/bibliotheque/model/Book.java
@@ -0,0 +1,48 @@
+package cda.bibliotheque.model;
+
+import java.time.LocalDate;
+
+public class Book {
+    private int id;
+    private String title;
+    private LocalDate date;
+    private boolean isAvailable;
+
+    public Book() {
+    }
+
+    public Book (int id, String title, LocalDate date, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public String getTitle () {
+        return title;
+    }
+
+    public LocalDate getDate () {
+        return date;
+    }
+
+    public boolean getIsAvailable () {
+        return isAvailable;
+    }
+
+    public void setTile (String title) {
+        this.title = title;
+    }
+
+    public void setDate (LocalDate date) {
+        this.date = date;
+    }
+
+    public void setIsAvailable (boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+}
