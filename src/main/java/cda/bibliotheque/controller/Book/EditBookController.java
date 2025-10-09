3c452517a830b692ec3e1bc4358acd17b905b810
QuentinG <ZexFrost1@gmail.com>
2025-10-09 10:31:57 +0200

    [+] Added update and delete actions

--- src/main/java/cda/bibliotheque/controller/Book/EditBookController.java
+++ src/main/java/cda/bibliotheque/controller/Book/EditBookController.java
@@ -1,5 +1,57 @@
 package cda.bibliotheque.controller.Book;
 
+import java.io.IOException;
+
+import javafx.beans.property.ObjectProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import cda.bibliotheque.model.Book;
+import cda.bibliotheque.dao.BookDAO;
+import javafx.beans.property.SimpleObjectProperty;
+import cda.bibliotheque.App;
+
 public class EditBookController {
-    
+
+    private final ObjectProperty<Book> book = new SimpleObjectProperty<>();
+    private final BookDAO bookDAO = new BookDAO();
+
+    @FXML
+    private ChoiceBox<String> colIsAvailable;
+
+    @FXML
+    private DatePicker inputReleaseDate;
+
+    @FXML
+    private TextField inputTitle;
+
+    @FXML
+    public void initialize() {
+        colIsAvailable.getItems().addAll("Disponible", "Pas disponible");
+        book.addListener((obs, oldBook, newBook) -> {
+            if (newBook != null) {
+                inputReleaseDate.setValue(newBook.getDate());
+                inputTitle.setText(newBook.getTitle());
+            }
+        });
+    }
+
+    @FXML
+    void submit(ActionEvent event) throws IOException {
+        Book newBook = book.get();
+        newBook.setTile(inputTitle.getText());
+        newBook.setDate(inputReleaseDate.getValue());
+        newBook.setIsAvailable(colIsAvailable.getValue() == "Disponible" ? true : false);
+        bookDAO.updateBook(newBook);
+        App.setRoot("books/books");
+    }
+
+    public EditBookController() {}
+
+    public void setBook (Book book) {
+        this.book.set(book);
+    }
+
 }
--- src/main/java/cda/bibliotheque/controller/Book/ReadBookController.java
+++ src/main/java/cda/bibliotheque/controller/Book/ReadBookController.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import cda.bibliotheque.dao.BookDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import cda.bibliotheque.model.Book;
@@ -14,6 +15,10 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import cda.bibliotheque.App;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.Parent;
 
 public class ReadBookController {
 
@@ -22,6 +27,9 @@ public class ReadBookController {
 
     @FXML
     private TableColumn<Book, String> colIsAvailable;
+
+    @FXML
+    private TableColumn<Book, Void> colActions;
 
     @FXML
     private TableColumn<Book, LocalDate> colReleaseDate;
@@ -37,7 +45,46 @@ public class ReadBookController {
         colTitle.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getTitle()));
         colIsAvailable.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getIsAvailable() ? "Disponible" : "Pas disponible"));
         colReleaseDate.setCellValueFactory(cell -> new SimpleObjectProperty<LocalDate>(cell.getValue().getDate()));
-        
+        colActions.setCellFactory(cell -> new TableCell<>() {
+            private final Button buttonEdit = new Button("Modifier");
+            private final Button buttonDelete = new Button("Supprimer");
+            private final HBox box = new HBox(10, buttonEdit, buttonDelete);
+            {
+                buttonEdit.setOnAction(event -> {
+                    int index = getIndex();
+                    Book bookToEdit = booksTable.getItems().get(index);
+                    try {
+                        FXMLLoader loader = new FXMLLoader(App.class.getResource("books/edit-book.fxml"));
+                        Parent parent = loader.load();
+
+                        EditBookController editBookController = loader.getController();
+                        editBookController.setBook(bookToEdit);
+
+                        App.getScene().setRoot(parent);
+                    } catch (IOException e) {
+                        System.out.println(e.getCause());
+                        System.err.println("Erreur lors de la création du bouton edit ->" + e);
+                    }
+                });
+
+                buttonDelete.setOnAction(event -> {
+                    int index = getIndex();
+                    Book bookToDelete = booksTable.getItems().get(index);
+                    bookDAO.deleteAuthor(bookToDelete.getId());
+                    loadBooks();
+                });
+            }
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(box);
+                }
+            }
+        });
+
         loadBooks();
     }
 
